05d0e9990bc0298bbbdf2fb0853337d0ad1d869f
Jake <Jake@STU17-68.catlin.edu>
2015-01-23 10:57:31 -0800

    added dieggnostics

---
+++ Dieggnostics/src/edu/team1540/recycle/RecyclingActivity.java
@@ -0,0 +1,12 @@
+package edu.team1540.recycle;
+
+import edu.team1540.egg.core.FragmentBasket;
+import edu.team1540.egg.core.ScoutingActivity;
+
+public class RecyclingActivity extends ScoutingActivity {
+
+	@Override
+	public FragmentBasket[] getPages() {
+		return null;
+	}
+}
