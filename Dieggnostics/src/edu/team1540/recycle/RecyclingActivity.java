d2abf90f5c14c0ce73d52365d80a52fdf7ae5d04
Jake <Jake@STU17-68.catlin.edu>
2015-01-23 12:45:28 -0800

    new activity & basket

--- Dieggnostics/src/edu/team1540/recycle/RecyclingActivity.java
+++ Dieggnostics/src/edu/team1540/recycle/RecyclingActivity.java
@@ -2,11 +2,12 @@ package edu.team1540.recycle;
 
 import edu.team1540.egg.core.FragmentBasket;
 import edu.team1540.egg.core.ScoutingActivity;
+import edu.team1540.recycle.basket.home.HomeFragment;
 
 public class RecyclingActivity extends ScoutingActivity {
 
 	@Override
 	public FragmentBasket[] getPages() {
-		return null;
+		return new FragmentBasket[] { new FragmentBasket(this, "Home", new HomeFragment()) };
 	}
 }
---
+++ Dieggnostics/src/edu/team1540/recycle/basket/home/HomeFragment.java
@@ -0,0 +1,12 @@
+package edu.team1540.recycle.basket.home;
+
+import edu.team1540.egg.core.ScoutingFragment;
+import edu.team1540.recycle.R;
+
+public class HomeFragment extends ScoutingFragment {
+
+	public HomeFragment() {
+		super(R.layout.home_fragment_home);
+	}
+
+}
