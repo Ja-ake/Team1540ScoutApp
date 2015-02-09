528ee414b1d5cd1b7a30cd90c147d676fe2ed018
Gregor Peach <gregorpeach@gmail.com>
2015-02-09 13:46:51 -0800

    Cacheing and database work.

--- Incubator/src/org/team1540/incubator/HandlerManager.java
+++ Incubator/src/org/team1540/incubator/HandlerManager.java
@@ -3,8 +3,10 @@ package org.team1540.incubator;
 import org.team1540.common.core.bluetooth.Address;
 import org.team1540.common.core.bluetooth.ConnectedThreadHandler;
 import org.team1540.common.core.schema.SchemaTransmitionHandler;
+import org.team1540.common.core.schema.impl.StandSchema;
 import org.team1540.common.core.schema.impl.TestSchema;
 import org.team1540.common.system.ConnectionHandler;
+import org.team1540.incubator.handler.StandHandler;
 import org.team1540.incubator.handler.TestHandler;
 
 public class HandlerManager {
@@ -12,6 +14,7 @@ public class HandlerManager {
 
 	public HandlerManager() {
 		schemaHandler.register(TestSchema.class, new TestHandler());
+		schemaHandler.register(StandSchema.class, new StandHandler());
 	}
 
 	public void register(final ConnectionHandler h, final Address a) {
